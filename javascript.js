0c564389ab4cfd5d731f12ddcee247c25d293109
Krym <95751922+Krymvee@users.noreply.github.com>
2023-03-15 14:59:10 +0700

    wis

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function merah(){
+    document.getElementById('container').style.backgroundColor = "red";
+}
+
+function hijau(){
+    document.getElementById('container').style.backgroundColor = "green";
+}
+
+function biru(){
+    document.getElementById('container').style.backgroundColor = "blue";
+}
+
+function reset(){
+    document.getElementById('container').style.backgroundColor = "white";
+}
